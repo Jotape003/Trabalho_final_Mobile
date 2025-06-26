8c8c2ec99b1ef04b946a4a2ec2ecda7ab0c83f35
Joao Pedro <jpedrosm2704@gmail.com>
2025-06-26 11:59:03 -0300

    Notificações de Eventos implementado + verificação do Switch de Notificações DENOVO

---
+++ Edumi/app/src/main/java/com/example/edumi/notifications/NotificationsReceiver.kt
@@ -0,0 +1,34 @@
+package com.example.edumi.notifications
+
+import android.Manifest
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
+import com.example.edumi.R
+
+class NotificationReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent?) {
+
+        val titulo = intent?.getStringExtra("TITULO") ?: "Evento: "
+        val id = intent?.getIntExtra("ID_EVENTO", 0) ?: 0
+
+        val notification = NotificationCompat.Builder(context, "EVENTOS_CHANNEL")
+            .setSmallIcon(R.drawable.responsavel)
+            .setContentTitle("Lembrete de Evento!")
+
+            .setContentText(titulo)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .build()
+
+        if (ContextCompat.checkSelfPermission(
+                context,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            NotificationManagerCompat.from(context).notify(id, notification)
+        }    }
+}
