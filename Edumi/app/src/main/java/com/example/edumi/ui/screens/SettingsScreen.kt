d255d06c4b21044ae590eaec6012df5464b203d0
Joao Pedro <jpedrosm2704@gmail.com>
2025-06-26 10:30:52 -0300

    Notificações de Eventos implementado + verificação do Switch de Notificações

--- Edumi/app/src/main/java/com/example/edumi/ui/screens/SettingsScreen.kt
+++ Edumi/app/src/main/java/com/example/edumi/ui/screens/SettingsScreen.kt
@@ -21,13 +21,17 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import com.example.edumi.models.eventos
+import com.example.edumi.notifications.agendarNotificacaoEvento
+import com.example.edumi.notifications.cancelarNotificacaoEvento
 
 @Composable
 fun SettingsScreen(onThemeToggle: () -> Unit) {
     var isDarkModeEnabled by remember { mutableStateOf(false) }
     var isNotificationsEnabled by remember { mutableStateOf(false) }
-
+    val context = LocalContext.current
 
     Column(
         modifier = Modifier
@@ -78,8 +82,17 @@ fun SettingsScreen(onThemeToggle: () -> Unit) {
 
             Switch(
                 checked = isNotificationsEnabled,
-                onCheckedChange = {
-                    isNotificationsEnabled = it
+                onCheckedChange = { enable ->
+                    isNotificationsEnabled = enable
+                    if (enable){
+                        eventos.forEach { evento ->
+                            agendarNotificacaoEvento(context, evento)
+                        }
+                    } else {
+                        eventos.forEach { evento ->
+                            cancelarNotificacaoEvento(context, evento)
+                        }
+                    }
                 }
             )
         }
